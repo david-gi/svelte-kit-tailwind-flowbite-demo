a948b93378858fb7c20e18887b519a1495991b5f
Recursifist <DavidGiag@protonmail.com>
2022-12-18 15:54:34 +0100

    Basic table component

---
+++ src/lib/components/BasicTable.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    import { Table, TableBody, TableBodyRow, TableHead } from 'flowbite-svelte'
+    import type { PromiseStatus } from '$src/models/base'
+
+    export let status: PromiseStatus
+    export let hoverable: boolean
+    export let striped: boolean
+</script>
+
+<Table {hoverable} {striped} color="purple">
+    <TableHead>
+        <slot name="header" />
+    </TableHead>
+    <TableBody>
+        {#if status == 'result'}
+            <slot name="body" />
+        {:else}
+            <TableBodyRow>
+                <td class="p-6" colspan="10">
+                    {status == 'pending' ? 'Loading...' : status}
+                </td>
+            </TableBodyRow>
+        {/if}
+    </TableBody>
+</Table>
